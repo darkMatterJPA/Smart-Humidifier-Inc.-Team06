7af2397cb6c9910003268667efae9ecaa43588d2
Stacey <Sllawson0@yahoo.com>
2019-09-05 09:18:14 -0400

    Added humidity text, drop down menu for humidity settings
    Need to check code - app crashing when run

--- HumdifierApp/app/src/main/java/com/example/humidifier/MainActivity.java
+++ HumdifierApp/app/src/main/java/com/example/humidifier/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.humidifier;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initiate a Switch
+        Switch simpleSwitch = findViewById(R.id.onswitch);
+        simpleSwitch.setTextOn("On"); // displayed text of the Switch whenever it is in checked or on state
+        simpleSwitch.setTextOff("Off"); // displayed text of the Switch whenever it is in unchecked i.e. o
+//set the current state of a Switch
+        simpleSwitch.setChecked(true);
+
+        String[] humidity = {"Low", "Medium", "High"};
+
+        Spinner spin = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, humidity);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
     }
 }
