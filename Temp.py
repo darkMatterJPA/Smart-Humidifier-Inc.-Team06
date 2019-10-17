de483db471529c6b36cb79d717dedb8a60c61470
Alton Panton <alton.panton@gmail.com>
2019-10-17 16:23:01 -0400

    sample client code

--- Temp.py
+++ Temp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import socketio
+import json
 
 sio = socketio.Client()
 sio.connect('https://polar-meadow-51053.herokuapp.com/')
@@ -10,3 +11,12 @@ print('my sid is: ', sio.sid)
 def on_message(data):
 	print('I recieved a Message: ', data['hello'])
 
+@sio.on('power-status-and-humidity-Setting-to-pi')
+def on_powerStatusHumidity(data):
+	''' Do some stuff '''
+
+@sio.on('schedule-to-pi')
+def on_schedule(data):
+	''' Do some stuff '''
+
+# sio.emit('waterLevel-from-pi',''' some data ''')
